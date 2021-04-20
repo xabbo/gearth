bfc4caa145b71ca263906ae0044eff8f403c7fea
b7c <58299468+b7c@users.noreply.github.com>
2021-04-20 22:22:11 +1200

    Add extension base class

---
+++ Xabbo.GEarth/GEarthExtension.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+using Xabbo.Messages;
+using Xabbo.Interceptor;
+using Xabbo.Interceptor.GEarth;
+
+namespace Xabbo.GEarth
+{
+    /// <summary>
+    /// A base implementation for a G-Earth extension.
+    /// </summary>
+    public abstract class GEarthExtension : INotifyPropertyChanged
+    {
+        protected bool Set<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value))
+            {
+                return false;
+            }
+            else
+            {
+                field = value;
+                RaisePropertyChanged(propertyName);
+                return true;
+            }
+        }
+
+        protected virtual void RaisePropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public IMessageManager Messages { get; }
+        public GEarthRemoteInterceptor Interceptor { get; }
+
+        public Incoming In => Messages.In;
+        public Outgoing Out => Messages.Out;
+
+        public bool IsInterceptorConnected => Interceptor.IsConnected;
+        public bool IsGameConnected => Interceptor.IsGameConnected;
+
+        /// <summary>
+        /// Sends a message to the server.
+        /// </summary>
+        public Task SendAsync(Header header, params object[] values) => Interceptor.SendToServerAsync(header, values);
+        /// <summary>
+        /// Sends a message to the server.
+        /// </summary>
+        public Task SendAsync(IReadOnlyPacket packet) => Interceptor.SendToServerAsync(packet);
+        /// <summary>
+        /// Sends a message to the client.
+        /// </summary>
+        public Task SendToClientAsync(Header header, params object[] values) => Interceptor.SendToClientAsync(header, values);
+        /// <summary>
+        /// Sends a message to the client.
+        /// </summary>
+        public Task SendToClientAsync(IReadOnlyPacket packet) => Interceptor.SendToClientAsync(packet);
+
+        public GEarthExtension(GEarthOptions options, int port)
+        {
+            Messages = new UnifiedMessageManager("messages.ini");
+            Interceptor = new GEarthRemoteInterceptor(Messages, options, port);
+
+            Interceptor.InterceptorConnected += OnInterceptorConnected;
+            Interceptor.Initialized += OnInterceptorInitialized;
+            Interceptor.Clicked += OnClicked;
+            Interceptor.Connected += OnGameConnected;
+            Interceptor.Intercepted += OnIntercepted;
+            Interceptor.Disconnected += OnGameDisconnected;
+            Interceptor.InterceptorDisconnected += OnInterceptorDisconnected;
+
+            Interceptor.Start();
+        }
+
+        protected virtual void OnInterceptorConnected(object? sender, EventArgs e)
+        {
+            RaisePropertyChanged(nameof(IsInterceptorConnected));
+        }
+        protected virtual void OnInterceptorInitialized(object? sender, EventArgs e) { }
+        protected virtual void OnClicked(object? sender, EventArgs e) { }
+        protected virtual void OnGameConnected(object? sender, GameConnectedEventArgs e)
+        {
+            Interceptor.Dispatcher.Bind(this);
+            RaisePropertyChanged(nameof(IsGameConnected));
+        }
+        protected virtual void OnGameDisconnected(object? sender, EventArgs e)
+        {
+            RaisePropertyChanged(nameof(IsGameConnected));
+        }
+        protected virtual void OnIntercepted(object? sender, InterceptArgs e) { }
+        protected virtual void OnInterceptorDisconnected(object? sender, EventArgs e)
+        {
+            RaisePropertyChanged(nameof(IsInterceptorConnected));
+            RaisePropertyChanged(nameof(IsGameConnected));
+        }
+    }
+}
