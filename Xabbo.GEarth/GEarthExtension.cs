f03bcbc80df74df845b66695a89698c81f5af1c3
b7c <58299468+b7c@users.noreply.github.com>
2021-08-28 14:44:35 +1200

    update

--- Xabbo.GEarth/GEarthExtension.cs
+++ Xabbo.GEarth/GEarthExtension.cs
@@ -12,8 +12,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
-using Microsoft.Extensions.Configuration;
-
 using Xabbo.Messages;
 using Xabbo.Interceptor;
 using Xabbo.Interceptor.Dispatcher;
@@ -21,18 +19,15 @@ using Xabbo.Interceptor.Dispatcher;
 namespace Xabbo.GEarth
 {
     /// <summary>
-    /// A <see cref="IRemoteInterceptor"/> implementation for G-Earth extensions.
+    /// A <see cref="IRemoteInterceptor"/> implementation for G-Earth.
     /// </summary>
     public class GEarthExtension : IRemoteInterceptor, INotifyPropertyChanged
     {
-        private const int
-            DEFAULT_PORT = 9092,
-            CONNECT_INTERVAL = 1000;
+        private const int CONNECT_INTERVAL = 1000;
 
-        private static readonly Encoding _encoding = Encoding.Latin1;
         private static readonly byte[]
-            _toClientBytes = _encoding.GetBytes("TOCLIENT"),
-            _toServerBytes = _encoding.GetBytes("TOSERVER");
+            _toClientBytes = Encoding.ASCII.GetBytes("TOCLIENT"),
+            _toServerBytes = Encoding.ASCII.GetBytes("TOSERVER");
 
         private enum GIncoming : short
         {
@@ -82,18 +77,27 @@ namespace Xabbo.GEarth
 
         public event PropertyChangedEventHandler? PropertyChanged;
 
-        private readonly SemaphoreSlim _sendSemaphore = new(1, 1);
+        private readonly int _remotePort;
 
-        private TcpClient? _client;
+        private readonly SemaphoreSlim _sendSemaphore = new(1, 1);
+        private readonly Memory<byte> _buffer = new byte[6];
+
+        private TcpClient? _tcpClient;
         private NetworkStream? _ns;
 
         private CancellationTokenSource? _cancellation;
 
+        #region - Events -
+        public event EventHandler<ConnectionFailedEventArgs>? InterceptorConnectionFailed;
+        protected virtual void OnInterceptorConnectionFailed(ConnectionFailedEventArgs e)
+            => InterceptorConnectionFailed?.Invoke(this, e);
+
         public event EventHandler? InterceptorConnected;
         protected virtual void OnInterceptorConnected() => InterceptorConnected?.Invoke(this, EventArgs.Empty);
 
-        public event EventHandler? InterceptorDisconnected;
-        protected virtual void OnInterceptorDisconnected() => InterceptorDisconnected?.Invoke(this, EventArgs.Empty);
+        public event EventHandler<DisconnectedEventArgs>? InterceptorDisconnected;
+        protected virtual void OnInterceptorDisconnected(DisconnectedEventArgs e)
+            => InterceptorDisconnected?.Invoke(this, e);
 
         public event EventHandler<InterceptorInitializedEventArgs>? Initialized;
         protected virtual void OnInitialized(InterceptorInitializedEventArgs e) => Initialized?.Invoke(this, e);
@@ -109,81 +113,125 @@ namespace Xabbo.GEarth
 
         public event EventHandler? Clicked;
         protected virtual void OnClicked() => Clicked?.Invoke(this, EventArgs.Empty);
+        #endregion
 
-        public int Port { get; private set; }
+        /// <summary>
+        /// Gets the options used by this extension.
+        /// </summary>
         public GEarthOptions Options { get; }
 
+        /// <summary>
+        /// Gets the message manager used by this extension.
+        /// </summary>
         public IMessageManager Messages { get; }
+
+        /// <summary>
+        /// Gets the incoming headers from the message manager.
+        /// </summary>
+        public Incoming In => Messages.In;
+
+        /// <summary>
+        /// Gets the outgoing headers from the message manager.
+        /// </summary>
+        public Outgoing Out => Messages.Out;
+
+        /// <summary>
+        /// Gets the dispatcher responsible for routing intercepted messages.
+        /// </summary>
         public IInterceptDispatcher Dispatcher { get; }
-        public string ClientIdentifier { get; private set; } = string.Empty;
-        public ClientType ClientType { get; private set; }
 
         private bool _isRunning;
         public bool IsRunning
         {
             get => _isRunning;
-            set => Set(ref _isRunning, value);
+            private set => Set(ref _isRunning, value);
         }
 
         private bool _isInterceptorConnected;
         public bool IsInterceptorConnected
         {
             get => _isInterceptorConnected;
-            set => Set(ref _isInterceptorConnected, value);
+            private set => Set(ref _isInterceptorConnected, value);
+        }
+
+        private int _port;
+        public int Port
+        {
+            get => _port;
+            private set => Set(ref _port, value);
         }
 
         private bool _isConnected;
         public bool IsConnected
         {
             get => _isConnected;
-            set => Set(ref _isConnected, value);
+            private set => Set(ref _isConnected, value);
         }
 
-        public Incoming In => Messages.In;
-        public Outgoing Out => Messages.Out;
+        private string _clientIdentifier = string.Empty;
+        public string ClientIdentifier
+        {
+            get => _clientIdentifier;
+            private set => Set(ref _clientIdentifier, value);
+        }
+
+        private ClientType _client = ClientType.Unknown;
+        public ClientType Client
+        {
+            get => _client;
+            private set => Set(ref _client, value);
+        }
 
         public void Send(Header header, params object[] values) => SendAsync(header, values);
         public void Send(IReadOnlyPacket packet) => SendAsync(packet);
-        public Task SendAsync(Header header, params object[] values) => ForwardPacketAsync(Packet.Compose(ClientType, header, values));
+        public Task SendAsync(Header header, params object[] values) => ForwardPacketAsync(Packet.Compose(Client, header, values));
         public Task SendAsync(IReadOnlyPacket packet) => ForwardPacketAsync(packet);
 
-        public GEarthExtension(IConfiguration config, IMessageManager messages, GEarthOptions options)
+        /// <summary>
+        /// Creates a new <see cref="GEarthExtension"/> using the specified <see cref="IMessageManager"/> and <see cref="GEarthOptions"/>.
+        /// </summary>
+        /// <param name="messages">The message manager to be used by this extension.</param>
+        /// <param name="options">The options to be used by this extension.</param>
+        public GEarthExtension(IMessageManager messages, GEarthOptions options)
         {
             Messages = messages;
             Options = options;
 
-            Port = config.GetValue("Xabbo:Interceptor:Port", DEFAULT_PORT);
-
             Dispatcher = new InterceptDispatcher(messages);
         }
 
-        public GEarthExtension(IMessageManager messages, GEarthOptions options, int port)
-        {
-            Messages = messages;
-            Options = options;
+        /// <summary>
+        /// Creates a new GEarthExtension with the specified <see cref="GEarthOptions"/>.
+        /// Uses a <see cref="UnifiedMessageManager"/> as the message manager, which loads a file named <c>messages.ini</c>.
+        /// </summary>
+        /// <param name="options">The options to be used by this extension.</param>
+        public GEarthExtension(GEarthOptions options)
+            : this(new UnifiedMessageManager("messages.ini"), options)
+        { }
 
-            Port = port;
-
-            Dispatcher = new InterceptDispatcher(messages);
-        }
-
-        public GEarthExtension(GEarthOptions options, int port)
-        {
-            Messages = new UnifiedMessageManager("messages.ini");
-            Options = options;
-
-            Port = port;
-
-            Dispatcher = new InterceptDispatcher(Messages);
-        }
-
-        public Task RunAsync()
+        public async Task RunAsync()
         {
             if (IsRunning)
                 throw new InvalidOperationException("The interceptor service is already running.");
 
             _cancellation = new CancellationTokenSource();
-            return HandleInterceptorAsync(_cancellation.Token);
+
+            IsRunning = true;
+
+            try
+            {
+                await HandleInterceptorAsync(_cancellation.Token);
+            }
+            finally
+            {
+                Dispatcher.ReleaseAll();
+
+                _tcpClient?.Close();
+                _tcpClient = null;
+                _ns = null;
+
+                IsRunning = false;
+            }
         }
 
         public void Stop()
@@ -196,23 +244,38 @@ namespace Xabbo.GEarth
 
         private async Task HandleInterceptorAsync(CancellationToken cancellationToken)
         {
-            try
+            while (!cancellationToken.IsCancellationRequested)
             {
-                IsRunning = true;
+                Exception? error = null;
 
-                _client = await ConnectAsync(cancellationToken);
+                try
+                {
+                    _tcpClient = await ConnectAsync(cancellationToken);
 
-                IsInterceptorConnected = true;
-                OnInterceptorConnected();
+                    IsInterceptorConnected = true;
+                    OnInterceptorConnected();
 
-                Pipe pipe = new();
-                await Task.WhenAll(
-                    FillPipeAsync(_ns = _client.GetStream(), pipe.Writer),
-                    ProcessPipeAsync(pipe.Reader)
-                );
-            }
-            finally
-            {
+                    Pipe pipe = new();
+                    await Task.WhenAll(
+                        FillPipeAsync(_ns = _tcpClient.GetStream(), pipe.Writer, cancellationToken),
+                        ProcessPipeAsync(pipe.Reader, cancellationToken)
+                    );
+                }
+                catch (Exception ex)
+                {
+                    error = ex;
+                }
+                finally
+                {
+                    Dispatcher.ReleaseAll();
+
+                    _ns = null;
+                    _tcpClient?.Close();
+                    _tcpClient = null;
+                }
+
+                DisconnectedEventArgs disconnectedEventArgs = new(error);
+
                 if (IsInterceptorConnected)
                 {
                     if (IsConnected)
@@ -222,37 +285,45 @@ namespace Xabbo.GEarth
                     }
 
                     IsInterceptorConnected = false;
-                    OnInterceptorDisconnected();
+                    OnInterceptorDisconnected(disconnectedEventArgs);
                 }
 
-                IsRunning = false;
-                Dispatcher.ReleaseAll();
+                if (!disconnectedEventArgs.Reconnect)
+                {
+                    if (error is not null)
+                    {
+                        throw error;
+                    }
 
-                _client?.Close();
-                _client = null;
-                _ns = null;
+                    break;
+                }
             }
         }
 
         private async Task<TcpClient> ConnectAsync(CancellationToken cancellationToken)
         {
+            int attempt = 0;
+
             while (true)
             {
-                cancellationToken.ThrowIfCancellationRequested();
-
+                attempt++;
                 TcpClient client = new();
 
                 try
                 {
-                    await client.ConnectAsync(IPAddress.Loopback, Port, cancellationToken);
+                    await client.ConnectAsync(IPAddress.Loopback, Options.Port, cancellationToken);
+                    return client;
                 }
                 catch
                 {
-                    await Task.Delay(CONNECT_INTERVAL, cancellationToken);
-                    continue;
+                    ConnectionFailedEventArgs e = new(attempt);
+                    OnInterceptorConnectionFailed(e);
+
+                    if (!e.Retry)
+                        throw;
                 }
 
-                return client;
+                await Task.Delay(CONNECT_INTERVAL, cancellationToken);
             }
         }
 
@@ -284,16 +355,16 @@ namespace Xabbo.GEarth
             }
         }
 
-        private static async Task FillPipeAsync(Stream stream, PipeWriter writer)
+        private static async Task FillPipeAsync(Stream stream, PipeWriter writer, CancellationToken cancellationToken)
         {
             Exception? error = null;
 
             while (true)
             {
-                Memory<byte> memory = writer.GetMemory(512);
+                Memory<byte> memory = writer.GetMemory(1024);
                 try
                 {
-                    int bytesRead = await stream.ReadAsync(memory);
+                    int bytesRead = await stream.ReadAsync(memory, cancellationToken);
                     if (bytesRead == 0) break;
 
                     writer.Advance(bytesRead);
@@ -311,13 +382,13 @@ namespace Xabbo.GEarth
             await writer.CompleteAsync(error);
         }
 
-        private async Task ProcessPipeAsync(PipeReader reader)
+        private async Task ProcessPipeAsync(PipeReader reader, CancellationToken cancellationToken)
         {
             Exception? error = null;
 
             while (true)
             {
-                ReadResult result = await reader.ReadAsync();
+                ReadResult result = await reader.ReadAsync(cancellationToken);
 
                 ReadOnlySequence<byte> buffer = result.Buffer;
 
@@ -384,8 +455,8 @@ namespace Xabbo.GEarth
                     Options.Version,
                     Options.Description,
                     Options.ShowEventButton,
-                    !string.IsNullOrWhiteSpace(Options.FilePath),
-                    Options.FilePath,
+                    !string.IsNullOrWhiteSpace(Options.FileName),
+                    Options.FileName,
                     Options.Cookie,
                     Options.ShowLeaveButton,
                     Options.ShowDeleteButton
@@ -415,7 +486,7 @@ namespace Xabbo.GEarth
                 throw new InvalidOperationException("Invalid packet intercept data (insufficient delimiter bytes)");
 
             bool isBlocked = span[0] == '1';
-            int index = int.Parse(_encoding.GetString(span[(tabs[0] + 1)..tabs[1]]));
+            int index = int.Parse(Encoding.ASCII.GetString(span[(tabs[0] + 1)..tabs[1]]));
             bool isOutgoing = span[tabs[1] + 3] == 'S';
             bool isModified = span[tabs[2] + 1] == '1';
 
@@ -424,16 +495,16 @@ namespace Xabbo.GEarth
             ReadOnlySpan<byte> packetSpan = span[(tabs[2] + 2)..];
             short headerValue = BinaryPrimitives.ReadInt16BigEndian(packetSpan[4..6]);
 
-            if (!Messages.TryGetHeaderByValue(destination, ClientType, headerValue, out Header? header))
+            if (!Messages.TryGetHeaderByValue(destination, Client, headerValue, out Header? header))
             {
                 header = new Header(destination, headerValue);
             }
 
             return new InterceptArgs(
                 destination,
-                ClientType,
+                Client,
                 index,
-                new Packet(ClientType, header, packetSpan[6..])
+                new Packet(Client, header, packetSpan[6..])
             );
         }
 
@@ -454,7 +525,7 @@ namespace Xabbo.GEarth
             response.WriteByte((byte)(args.IsBlocked ? '1' : '0'));
             response.WriteByte(0x09);
 
-            response.WriteBytes(_encoding.GetBytes(args.Step.ToString()));
+            response.WriteBytes(Encoding.ASCII.GetBytes(args.Step.ToString()));
             response.WriteByte(0x09);
 
             response.WriteBytes(args.Destination == Destination.Client ? _toClientBytes : _toServerBytes);
@@ -462,7 +533,7 @@ namespace Xabbo.GEarth
 
             response.WriteByte((byte)((args.IsModified) ? '1' : '0'));
             response.WriteInt(2 + args.Packet.Length);
-            response.WriteShort(args.Packet.Header.GetValue(ClientType));
+            response.WriteShort(args.Packet.Header.GetValue(Client));
             response.WriteBytes(args.Packet.GetBuffer().Span);
 
             response.Position = 0;
@@ -484,9 +555,9 @@ namespace Xabbo.GEarth
             ClientIdentifier = packet.ReadString();
             string clientType = packet.ReadString();
 
-            if (clientType.StartsWith("Unity", StringComparison.OrdinalIgnoreCase)) ClientType = ClientType.Unity;
-            else if (clientType.StartsWith("Flash", StringComparison.OrdinalIgnoreCase)) ClientType = ClientType.Flash;
-            else ClientType = ClientType.Unknown;
+            if (clientType.StartsWith("Unity", StringComparison.OrdinalIgnoreCase)) Client = ClientType.Unity;
+            else if (clientType.StartsWith("Flash", StringComparison.OrdinalIgnoreCase)) Client = ClientType.Flash;
+            else Client = ClientType.Unknown;
 
             int n = packet.ReadInt();
             List<IClientMessageInfo> messages = new(n);
@@ -501,7 +572,7 @@ namespace Xabbo.GEarth
 
                 messages.Add(new ClientMessageInfo
                 {
-                    Client = ClientType,
+                    Client = Client,
                     Direction = isOutgoing ? Direction.Outgoing : Direction.Incoming,
                     Header = (short)id,
                     Name = name
@@ -509,7 +580,7 @@ namespace Xabbo.GEarth
             }
 
             Messages.LoadMessages(messages);
-            Dispatcher.Bind(this, ClientType);
+            Dispatcher.Bind(this, Client);
 
             IsConnected = true;
             OnConnected(new GameConnectedEventArgs()
@@ -518,7 +589,7 @@ namespace Xabbo.GEarth
                 Port = port,
                 ClientVersion = clientVersion,
                 ClientIdentifier = ClientIdentifier,
-                ClientType = ClientType,
+                ClientType = Client,
                 Messages = messages
             });
 
@@ -557,7 +628,7 @@ namespace Xabbo.GEarth
                     .WriteByte((byte)(packet.Header.IsOutgoing ? 1 : 0))
                     .WriteInt(6 + packet.Length) // length of (packet length + header + data)
                     .WriteInt(2 + packet.Length) // length of (header + data)
-                    .WriteShort(packet.Header.GetValue(ClientType))
+                    .WriteShort(packet.Header.GetValue(Client))
                     .WriteBytes(packet.GetBuffer().Span)
             );
         }
@@ -567,14 +638,14 @@ namespace Xabbo.GEarth
             NetworkStream? ns = _ns;
             if (ns is null) return;
 
-            Memory<byte> buffer = new byte[6];
-            BinaryPrimitives.WriteInt32BigEndian(buffer.Span[0..4], 2 + packet.Length);
-            BinaryPrimitives.WriteInt16BigEndian(buffer.Span[4..6], packet.Header.Value ?? -1);
+            short headerValue = packet.Header.Value ?? throw new Exception("Invalid packet header.");
 
             await _sendSemaphore.WaitAsync();
             try
             {
-                await ns.WriteAsync(buffer[0..6]);
+                BinaryPrimitives.WriteInt32BigEndian(_buffer.Span[0..4], 2 + packet.Length);
+                BinaryPrimitives.WriteInt16BigEndian(_buffer.Span[4..6], headerValue);
+                await ns.WriteAsync(_buffer[0..6]);
                 await ns.WriteAsync(packet.GetBuffer());
             }
             finally { _sendSemaphore.Release(); }
--- Xabbo.GEarth/GEarthOptions.cs
+++ Xabbo.GEarth/GEarthOptions.cs
@@ -1,31 +1,215 @@
 ﻿using System;
+using System.Reflection;
+
+using Microsoft.Extensions.Configuration;
 
 namespace Xabbo.GEarth
 {
+    /// <summary>
+    /// Specifies the options to be used by <see cref="GEarthExtension"/>.
+    /// </summary>
     public class GEarthOptions
     {
+        /// <summary>
+        /// The number of version fields to be included when converting the assembly version to a string for <see cref="Default"/>.
+        /// </summary>
+        public static int DefaultVersionFieldCount { get; set; } = 3;
+
+        /// <summary>
+        /// Gets the default options with the entry assembly's name and version.
+        /// </summary>
+        public static GEarthOptions Default
+        {
+            get
+            {
+                AssemblyName? assemblyName = Assembly.GetEntryAssembly()?.GetName();
+
+                return new GEarthOptions
+                {
+                    Title = assemblyName?.Name ?? "unknown",
+                    Version = assemblyName?.Version?.ToString(DefaultVersionFieldCount) ?? "?"
+                };
+            }
+        }
+
+        /// <summary>
+        /// The title of the extension.
+        /// </summary>
         public string Title { get; init; }
+        /// <summary>
+        /// The description of the extension.
+        /// </summary>
         public string Description { get; init; }
+        /// <summary>
+        /// The author of the extension.
+        /// </summary>
         public string Author { get; init; }
+        /// <summary>
+        /// The version of the extension.
+        /// </summary>
         public string Version { get; init; }
 
-        public bool ShowEventButton { get; init; }
-        public bool ShowLeaveButton { get; init; }
+        /// <summary>
+        /// Specifies whether to show the even (green play) button in G-Earth. Defaults to <c>true</c>.
+        /// </summary>
+        public bool ShowEventButton { get; init; } = true;
+        /// <summary>
+        /// Specifies whether to show the leave button in G-Earth. Defaults to <c>true</c>.
+        /// </summary>
+        public bool ShowLeaveButton { get; init; } = true;
+        /// <summary>
+        /// Specifies whether to show the delete button in G-Earth.
+        /// </summary>
         public bool ShowDeleteButton { get; init; }
 
-        public string FilePath { get; init; }
+        /// <summary>
+        /// The file path of the extension.
+        /// </summary>
+        public string FileName { get; init; }
+        /// <summary>
+        /// The cookie to be used for authentication.
+        /// </summary>
         public string Cookie { get; init; }
 
-        public bool IsInstalledExtension { get; init; }
+        /// <summary>
+        /// The port used to connect to G-Earth.
+        /// Defaults to 9092.
+        /// </summary>
+        public int Port { get; init; } = 9092;
 
+        /// <summary>
+        /// Creates a new <see cref="GEarthOptions"/> instance.
+        /// </summary>
         public GEarthOptions()
         {
             Title =
             Description =
             Author =
             Version =
-            FilePath =
+            FileName =
             Cookie = string.Empty;
+        }
+
+        /// <summary>
+        /// Creates a copy of the specified <see cref="GEarthOptions"/>.
+        /// </summary>
+        public GEarthOptions(GEarthOptions options)
+        {
+            Title = options.Title;
+            Description = options.Description;
+            Author = options.Author;
+            Version = options.Version;
+
+            ShowEventButton = options.ShowEventButton;
+            ShowLeaveButton = options.ShowLeaveButton;
+            ShowDeleteButton = options.ShowDeleteButton;
+
+            FileName = options.FileName;
+            Cookie = options.Cookie;
+
+            Port = options.Port;
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="GEarthOptions"/> with the <see cref="Title"/> changed.
+        /// </summary>
+        public GEarthOptions WithTitle(string title) => new(this) { Title = title };
+
+        /// <summary>
+        /// Creates a new <see cref="GEarthOptions"/> with the <see cref="Description"/> changed.
+        /// </summary>
+        public GEarthOptions WithDescription(string description) => new(this) { Description = description };
+
+        /// <summary>
+        /// Creates a new <see cref="GEarthOptions"/> with the <see cref="Author"/> changed.
+        /// </summary>
+        public GEarthOptions WithAuthor(string author) => new(this) { Author = author };
+
+        /// <summary>
+        /// Creates a new <see cref="GEarthOptions"/> with the <see cref="Version"/> changed.
+        /// </summary>
+        public GEarthOptions WithVersion(string version) => new(this) { Version = version };
+
+        /// <summary>
+        /// Creates a new <see cref="GEarthOptions"/> with <see cref="ShowEventButton"/> changed.
+        /// </summary>
+        public GEarthOptions WithShowEventButton(bool showEventButton) => new(this) { ShowEventButton = showEventButton };
+
+        /// <summary>
+        /// Creates a new <see cref="GEarthOptions"/> with <see cref="ShowLeaveButton"/> changed.
+        /// </summary>
+        public GEarthOptions WithShowLeaveButton(bool showLeaveButton) => new(this) { ShowLeaveButton = ShowLeaveButton };
+
+        /// <summary>
+        /// Creates a new <see cref="GEarthOptions"/> with <see cref="ShowDeleteButton"/> changed.
+        /// </summary>
+        public GEarthOptions WithShowDeleteButton(bool showDeleteButton) => new(this) { ShowDeleteButton = showDeleteButton };
+
+        /// <summary>
+        /// Creates a new <see cref="GEarthOptions"/> with the <see cref="FileName"/> changed.
+        /// </summary>
+        public GEarthOptions WithFilePath(string filePath) => new(this) { FileName = filePath };
+        /// <summary>
+        /// Creates a new <see cref="GEarthOptions"/> with the <see cref="Cookie"/> changed.
+        /// </summary>
+        public GEarthOptions WithCookie(string cookie) => new(this) { Cookie = cookie };
+
+        /// <summary>
+        /// Creates a new <see cref="GEarthOptions"/> with the <see cref="Port"/> changed.
+        /// </summary>
+        public GEarthOptions WithPort(int port) => new(this) { Port = port };
+
+        /// <summary>
+        /// Creates a new <see cref="GEarthOptions"/> with the specified arguments applied.
+        /// </summary>
+        public GEarthOptions WithArguments(string[] args)
+        {
+            int port = Port;
+            string cookie = Cookie;
+            string file = FileName;
+
+            for (int i = 0; i < args.Length - 1; i++)
+            {
+                switch (args[i])
+                {
+                    case "-c":
+                        cookie = args[++i];
+                        break;
+                    case "-f":
+                        file = args[++i];
+                        break;
+                    case "-p":
+                        string portArg = args[++i];
+                        if (!int.TryParse(portArg, out port))
+                            throw new FormatException($"Invalid port argument specified: '{portArg}'.");
+                        break;
+                    
+                }
+            }
+
+            return new GEarthOptions(this)
+            {
+                Port = port,
+                Cookie = cookie,
+                FileName = file
+            };
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="GEarthOptions"/> with the specified configuration applied.
+        /// </summary>
+        public GEarthOptions WithConfiguration(IConfiguration configuration)
+        {
+            int port = configuration.GetValue("Xabbo:Interceptor:Port", Port);
+            string cookie = configuration.GetValue("Xabbo:Interceptor:Cookie", Cookie);
+            string file = configuration.GetValue("Xabbo:Interceptor:File", FileName);
+
+            return new GEarthOptions(this)
+            {
+                Port = port,
+                Cookie = cookie,
+                FileName = file
+            };
         }
     }
 }
