3a94b709bb908cd68faa47160967681d0f64bf51
rob <rob@b7c.io>
2024-08-22 18:42:26 +1200

    remove example

--- src/Xabbo.GEarth.Example/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using Xabbo.GEarth.Example;
-
-Console.ForegroundColor = ConsoleColor.Yellow;
-Console.WriteLine("> Running extension...\n");
-Console.ResetColor();
-
-await new ExampleExtension(args).RunAsync();
